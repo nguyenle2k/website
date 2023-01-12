748968146ea99db6daed2f4f8a9b043948dfcd7b
Nguyên Lê <nle372999@gmail.com>
2023-01-12 14:45:49 +0700

    add cpn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routing/component/home/home.component';
+import { FooterComponent } from './routing/component/footer/footer.component';
+import { LongformComponent } from './routing/pages/longform/longform.component';
+import { MainComponent } from './routing/component/main/main.component';
+import { PlaceholderComponent } from './routing/pages/placeholder/placeholder.component';
+import { Longform1Component } from './routing/pages/longform1/longform1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    LongformComponent,
+    MainComponent,
+    PlaceholderComponent,
+    Longform1Component
   ],
   imports: [
     BrowserModule,
