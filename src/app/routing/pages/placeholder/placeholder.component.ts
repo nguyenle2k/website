fbf86c8402c58b5f37c6b013c0c631c38bf557a7
Nguyên Lê <nle372999@gmail.com>
2023-01-17 15:18:44 +0700

    add(placeholder): list

--- src/app/routing/pages/placeholder/placeholder.component.ts
+++ src/app/routing/pages/placeholder/placeholder.component.ts
@@ -8,3 +8,144 @@ import { Component } from '@angular/core';
 export class PlaceholderComponent {
 
 }
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { PostService } from '@services/api';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-longform4',
+  templateUrl: './longform4.component.html',
+  styleUrls: ['./longform4.component.less'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class Longform4Component implements OnInit {
+  public blogData: any;
+  private subRouter$!: Subscription;
+  public valuelanguage: any;
+  items = [
+    {
+      styles: 1,
+      title1: 'BƯỚC ĐỘT PHÁ',
+      title2: 'Ở DYNAMIC ISLAND',
+      image: './assets/images/longform4/center.png',
+      contents: [
+        {
+          content1:
+            'Apple dần tách xa các phiên bản của cùng một dòng đời iPhone. Ngoài việc có pin, màn hình lớn cùng đầy đủ camera, iPhone 14 Pro Max là lựa chọn được trang bị vi xử lý mạnh nhất của Apple cùng Dynamic Island. Điều này khiến sản phẩm trở thành món hàng được người dùng săn đón. Trong khi đó, iPhone 14 hoặc 14 Plus lại bị ghẻ lạnh.',
+        },
+        {
+          content1:
+            'Trong vòng 2 tuần, có hơn 80.000 chiếc iPhone 14 Pro Max được người dùng Việt Nam tiêu thụ. Đây là con số khổng lồ với một mẫu điện thoại cao cấp, giá trên 30 triệu đồng. Apple thu về hơn 2.800 tỷ đồng trong thời gian ngắn. Một phần thành công này đến từ thay đổi của thiết kế màn hình.',
+        },
+        {
+          content2:
+            'Táo khuyết giới thiệu kiểu khuyết đỉnh (notch) từ 2017. iFan phải đợi 5 năm để công ty này có cập nhật cho mặt trước của thiết bị. Thay vì “tai thỏ” lớn, iPhone 14 Pro Max sở hữu phần khuyết bé hơn, dạng viên thuốc trên đỉnh máy.',
+        },
+        {
+          content2:
+            'Apple che lấp mảng đen này bằng phần mềm. Táo khuyết gọi đó là Dynamic Island. Điểm hấp dẫn nhất của tính năng nằm ở hoạt ảnh. Apple xử lý nó rất mượt mà và thu hút. Hiệu ứng khi mở Face ID, có cuộc gọi đến, ghi âm hay kết nối AirPods không giống nhau. Khi đã quen, nó tạo ra thói quen nhìn vào đỉnh màn hình để kiểm soát trạng thái chiếc máy.',
+        },
+        {
+          content2:
+            'Về mặt thẩm mỹ, Dynamic Island chiếm nhiều diện tích trên màn hình hơn chỉ một chấm đen của Xiaomi 12 Pro, Find X5 Pro hay Z Fold4. Tuy nhiên, các tính năng phủ lên phần đảo động giúp người dùng quên đi đây là một phần khiếm khuyết.',
+        },
+      ],
+    },
+
+    {
+      styles: 1,
+      title1: 'THẾ MẠNH TRUYỀN THỐNG',
+      title2: 'CỦA APPLE',
+      image: './assets/images/longform4/center2.png',
+      contents: [
+        {
+          content1:
+            'Dynamic Island là chức năng mới, khiến những người dùng Apple muốn nâng cấp lên iPhone 14 Pro Max. Trong khi đó, những tính năng khác trên máy là điểm Táo khuyết làm cho khách hàng không thể rời khỏi hệ sinh thái của mình.',
+        },
+        {
+          content1:
+            'iPhone 14 Pro Max vẫn là chiếc máy có chất lượng hoàn thiện hàng đầu trên thị trường. Khung thép, mặt kính Ceramic Shield là mang đến độ bền bỉ, chắc chắn cho thiết bị. Cùng là máy cao cấp, nhưng các chi tiết nhỏ như lỗ loa, khe cắm SIM, mép kính tiếp giáp khung viền trên iPhone có chất lượng chế tạo, lắp ráp tốt hơn đối thủ Android ngang giá.',
+        },
+        {
+          content2:
+            'Hệ điều hành iOS ổn định, hỗ trợ thao tác mượt mà, dễ sử dụng là một điểm mạnh khác. iOS 16 cung cấp thêm các tính năng mới về quyền riêng tư, màn hình khóa, iMessage. Apple cập nhật nhiều năm cho tất cả iPhone của hãng. Tuy nhiên, để sử dụng đầy đủ tính năng, ổn định, người dùng cần sắm các mẫu máy đời mới.',
+        },
+        {
+          content2:
+            'Hệ điều hành iOS ổn định, hỗ trợ thao tác mượt mà, dễ sử dụng là một điểm mạnh khác. iOS 16 cung cấp thêm các tính năng mới về quyền riêng tư, màn hình khóa, iMessage. Apple cập nhật nhiều năm cho tất cả iPhone của hãng. Tuy nhiên, để sử dụng đầy đủ tính năng, ổn định, người dùng cần sắm các mẫu máy đời mới.',
+        },
+        {
+          content2:
+            'iPhone còn là trung tâm của hệ sinh thái nhà thông minh HomeKit. Hiện tại, có hàng trăm công ty sản xuất thiết bị theo tiêu chuẩn này của Apple. So với Google Home hay Amazon Alexa, HomeKit được đánh giá cao nhờ sự ổn định trong hệ thống, chất lượng thiết bị ngoại vi. Tính năng Shortcut trên iPhone còn giúp tự động, tối ưu hóa các ngữ cảnh sử dụng của người dùng với nhà thông minh.',
+        },
+        {
+          content2:
+            'Điểm trừ của iPhone 14 Pro Max là trợ lý ảo Siri vẫn chưa hỗ trợ ngôn ngữ tiếng Việt. Do đó, việc ra lệnh bằng giọng nói của người dùng trong nước chưa thuận lợi.',
+        },
+      ],
+    },
+
+    {
+      styles: 2,
+      title1: 'TIÊU CHUẨN CỦA',
+      title2: 'FLAGSHIP HIỆN ĐẠI',
+      image: './assets/images/longform4/center3.png',
+      contents: [
+        {
+          content1:
+            'Apple thường không phải là công ty chạy theo thông số. Nhưng iPhone 14 Pro Max là một thiết bị khác lạ với nhiều thay đổi về phần cứng từ Apple. Chiếc máy thiết lập ra một tiêu chuẩn mới cho những máy cao cấp trong năm nay.',
+        },
+        {
+          content2:
+            'Sau nhiều năm, Apple chính thức đưa camera chính iPhone lên mức 48 MP. Độ phân giải cao giúp độ chi tiết, khả năng thu sáng của máy được nâng cấp đáng kể. 48 MP còn phục vụ cho tính năng thu phóng mới, giả lập ở mức 2X. Như vậy, với 3 camera, người dùng iPhone 14 Pro Max sở hữu 4 mức zoom từ 0,5x, 1x, 2x và 3x.',
+        },
+        {
+          content2:
+            'Con chip A16 Bionic nới rộng khoảng cách hiệu năng giữa Apple với các thương hiệu Android. Đây là chiếc máy hiếm hoi trên thị trường có thể chơi tốt tựa game thế giới mở Genshin Impact ở mức 60 khung hình/giây. Nhiều trò chơi phổ biến cũng đã hỗ trợ 120 FPS cho thiết bị này.',
+        },
+      ],
+    },
+  ];
+
+  constructor(
+    private service: PostService,
+    protected route: ActivatedRoute,
+    protected translate: TranslateService,
+    protected router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    const { slug } = this.route.snapshot.params;
+    this.getDetailBySlug(slug);
+    this.subRouter$ = this.translate.onDefaultLangChange.subscribe(
+      (event: LangChangeEvent) => {
+        this.valuelanguage = event.lang;
+        let slugg = '';
+        if (event.lang == 'en') {
+          var arren = this.blogData?.slugTranslations.filter((item: any) => {
+            return item?.language == 'en';
+          });
+          slugg = arren[0]?.slug;
+        }
+        if (event.lang == 'vn') {
+          var arren = this.blogData?.slugTranslations.filter((item: any) => {
+            return item?.language == 'vn';
+          });
+          slugg = arren[0]?.slug;
+        }
+        setTimeout(() => {
+          this.router.navigate(['longform', slugg.toString()]);
+          this.getDetailBySlug(slugg);
+        }, 300);
+      },
+    );
+  }
+
+  getDetailBySlug(slug: string) {
+    this.service.getBySlug(slug).subscribe((res: any) => {
+      this.blogData = res.data;
+    });
+  }
+}
