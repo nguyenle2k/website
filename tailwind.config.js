7a43de8f295f99e2fe5050c469c79c84a5eb275f
Nguyên Lê <nle372999@gmail.com>
2023-01-30 08:56:15 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      container: {
-        padding: {
-          sm: '640px',
-          md: '789px',
-          lg: '1024px',
-          xl: '1220px',
-          '2xl': '1220px',
-        }
-      }
+    
     },
   },
   plugins: [],
